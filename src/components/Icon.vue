57b00acef8c3a8dcaeadc94f7165636e775ea40d
Nadine Baruth <nadine.baruth@entrados.de>
2023-09-27 11:21:10 +0200

    Add amenities section

---
+++ src/components/Icon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+// reactive props
+defineProps<{
+  variant: string
+}>()
+
+
+</script>
+
+<template>
+  <div>
+    <span class="mr-2">ICON</span>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+</style>
